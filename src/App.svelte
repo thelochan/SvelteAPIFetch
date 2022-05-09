0e878f979d37ff462c838eddb5a7da5cb3ba3abd
Neeraj <nrjvinod@yahoo.com>
2022-05-09 15:51:39 +1200

    remarks

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+import Hash from "./hash.svelte";
+
+	
+</script>
+
+<Hash />
+
+
+<style>
+	:root {
+
+		font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
+	}
+</style>
---
+++ src/hash.svelte
@@ -0,0 +1,23 @@
+<script lang='ts'>
+
+
+import { onMount } from "svelte";
+let promise;
+
+onMount(() => {
+    $: promise = fetch ("http://127.0.0.1:8000/api/v2/mine")
+})
+
+
+
+</script>
+
+{#await promise}
+<div>Wait</div>
+
+{:then results}
+<div>{JSON.stringify(results, null, 2)}</div>
+{:catch error}
+<div> {error.message}</div>
+{/await}
+
